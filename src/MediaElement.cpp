a8779a8188702db914b661748249e12353f6ca6d
Nsivaa <alberto.dicembre@gmail.com>
2025-03-25 19:15:42 +0000

    removed sleep and put while

--- src/MediaElement.cpp
+++ src/MediaElement.cpp
@@ -5,28 +5,23 @@ bool MediaElement::isVideo() const {
 }
 
 void MediaElement::generateThumbnail() {
+	if (!this->isVideo()) {
+		return;
+	}
+
     try {
 
-        if (this->isVideo()) {
-            ofVideoPlayer tempVideo;
-            tempVideo.load(this->videoPath);
-            tempVideo.update(); 
-            ofSleepMillis(30); // Small delay to allow decoding. Garbage but otherwise won't work
-            tempVideo.update();
-
-            if (tempVideo.isLoaded() && tempVideo.getPixels().isAllocated()) {
-                this->image.setFromPixels(tempVideo.getPixels());
-                this->image.resize(300, 300); // Resize to image size
-
-                // cout << "Thumbnail successfully created for: " << videoPath << endl;
-            }
-            else {
-                cout << "Failed to load first frame from video: " << videoPath << endl;
-            }
+        ofVideoPlayer tempVideo;
+        tempVideo.load(this->videoPath);
+        while (!tempVideo.isFrameNew()) {
+			tempVideo.update(); // Wait for the first frame to be loaded
         }
+            this->image.setFromPixels(tempVideo.getPixels());
+            this->image.resize(300, 300); // Resize to image size
+            // cout << "Thumbnail successfully created for: " << videoPath << endl;
     }
-	catch (const std::exception& e) {
-		cout << "Error generating thumbnail: " << e.what() << endl;
-	}
+    catch (const std::exception& e) {
+        cout << "Error generating thumbnail: " << e.what() << endl;
+    }
 }
 
